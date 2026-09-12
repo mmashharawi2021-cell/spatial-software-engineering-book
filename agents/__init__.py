56f03c7632b5051d9c97c5727ef6f1f22889636e
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-09-12 15:03:36 +0300

    Add geospatial agents package

---
+++ agents/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    SpatialDataAgent,
+    SpatialQueryAgent,
+    FieldDataAgent,
+    MapAssistantAgent,
+    GeoAIAssistant,
+)
+
+__all__ = [
+    "SpatialDataAgent",
+    "SpatialQueryAgent",
+    "FieldDataAgent",
+    "MapAssistantAgent",
+    "GeoAIAssistant",
+]
